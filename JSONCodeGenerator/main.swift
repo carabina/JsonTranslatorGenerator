0a969dee1f308984b715ba4afdda65063ca836dd
Vladyslav Zavalykhatko <vladyslav.zavalikhat@visma.com>
2017-10-20 10:31:45 +0200

    Initial Commit

---
+++ JSONCodeGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JSONCodeGenerator
+//
+//  Created by Vladyslav Zavalykhatko on 20/10/2017.
+//  Copyright © 2017 HatCom. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
